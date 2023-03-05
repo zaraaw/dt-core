2e29b49ebcaa2e82ef1d1b44f65d332ce2356435
zaraaw <63156182+zaraaw@users.noreply.github.com>
2023-03-05 17:49:11 +0100

    altered packages from "working listener"

---
+++ packages/LCT_subscriber/src/LCT_subscriber_node.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+import numpy as np
+import rospy
+
+from duckietown.dtros import DTROS, NodeType, TopicType, DTParam, ParamType
+from duckietown_msgs.msg import (LanePose)
+
+class LCTsubscriberNode(DTROS):
+    def __init__(self, node_name):
+
+        super(LCTsubscriberNode, self).__init__(node_name=node_name, node_type=NodeType.PERCEPTION)
+
+        # Initialize variables
+        self.pose_msg = LanePose()
+        self.current_pose_source = "lane_filter"
+
+        # Construct subscribers
+        self.sub_lane_reading = rospy.Subscriber("~lane_pose", LanePose, self.callback)
+        self.log("Initialized!")
+           
+    def callback(self,data):
+        rospy.loginfo("Heard: %s", data)
+          
+if __name__ == "__main__":
+    # Initialize the node
+    LCT_subscriber_node = LCTsubscriberNode(node_name="LCTsubscriber")
+    
+   
+    # Keep it spinning
+    rospy.spin()
+    
