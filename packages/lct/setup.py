2ad90aaca787e6c57df549eb3ba0da7d37d4a876
zaraaw <63156182+zaraaw@users.noreply.github.com>
2023-03-06 11:13:40 +0100

    Update and rename packages/LCT_subscriber/setup.py to packages/lct/setup.py

--- packages/lct/setup.py
+++ packages/lct/setup.py
@@ -6,7 +6,7 @@ from catkin_pkg.python_setup import generate_distutils_setup
 
 setup_args = generate_distutils_setup(
     packages=[
-        "LCT_subscriber",
+        "lct",
     ],
     package_dir={"": "include"},
 )
