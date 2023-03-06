7d3d570582f82ad52b49a517229c20ca5763a1f2
zaraaw <63156182+zaraaw@users.noreply.github.com>
2023-03-06 14:08:46 +0100

    Update lct_node.py

--- packages/lct/src/lct_node.py
+++ packages/lct/src/lct_node.py
@@ -1,9 +1,10 @@
 #!/usr/bin/env python3
 import numpy as np
 import rospy
+# import LCT 
 
 from duckietown.dtros import DTROS, NodeType, TopicType, DTParam, ParamType
-from duckietown_msgs.msg import (LanePose)
+from duckietown_msgs.msg import (LanePose, Twist2DStamped)
 
 class LCTNode(DTROS):
     def __init__(self, node_name):
@@ -14,24 +15,53 @@ class LCTNode(DTROS):
         self.pose_msg = LanePose()
         self.current_pose_source = "lane_filter"
 
+        # config file?
+
+
+
         # Construct subscribers
-        self.sub_lane_reading = rospy.Subscriber("~lane_pose", LanePose, self.callback)
+        self.sub_lane_reading = rospy.Subscriber("~lane_pose", LanePose, self.lanepose_callback)
+        self.sub_car_cmd = rospy.Subscriber("~car_cmd", Twist2DStamped, self.velocity_callback)
+
+
+
         self.log("Initialized!")
            
-    def callback(self,data):
+    def lanepose_callback(self, msg_lane_pose):
         #rospy.loginfo("Heard: %s", data)
 
         global d
         global phi
         global in_lane
 
-        d = data.d
-        phi = data.phi
-        in_lane = data.in_lane
+        d = msg_lane_pose.d
+        phi = msg_lane_pose.phi
+        in_lane = msg_lane_pose.in_lane
         
+        #rospy.loginfo("%s",d)
+        #rospy.loginfo("%s",phi)
+        #rospy.loginfo("%s",in_lane)
+    
+    def velocity_callback(self, msg_car_cmd):
+        # callback for velocity message
+
+        global v
+        v = msg_car_cmd.v
+
         rospy.loginfo("%s",d)
         rospy.loginfo("%s",phi)
         rospy.loginfo("%s",in_lane)
+
+        rospy.loginfo("velocity: %s", v)
+
+    """
+    def runLCT(self, d, phi, v):
+        cons = LCT.Constants()
+        cons.setConstants()
+        
+        LCT.runLCT().run_LCT(d, phi, v)
+    """           
+
           
 if __name__ == "__main__":
     # Initialize the node
@@ -41,3 +71,5 @@ if __name__ == "__main__":
     # Keep it spinning
     rospy.spin()
     
+# add to master.launch
+# <remap from="lct_node/car_cmd" to="lane_controller_node/car_cmd"/>
