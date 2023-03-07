8ec501852b15b32572e4e8952c4f7bd71d7c14ca
zaraaw <63156182+zaraaw@users.noreply.github.com>
2023-03-07 16:02:27 +0100

    Update LCT.py

--- packages/lct/src/LCT.py
+++ packages/lct/src/LCT.py
@@ -8,8 +8,8 @@ import random
 import copy
 import math
 import pandas as pd
+from gen_rule_pop import *
 
-#%% 
 # CONFIG FILE
 # Configuration file (setting the variables used later)
 
@@ -22,6 +22,7 @@ min_v = 3                           # choose a min value
 max_v = 5                           # check max value (assuming the ROS node would give the vel in m/s)
 stop_v = 0.5                        # the speed below which would be considered stopped/crashed
 
+# (prob wont use)
 # target_d = 0.05                   # some small value (assuming in m)
 # target_phi = 0.5                  # in rad 
 
@@ -31,32 +32,29 @@ best_fitness = 0
 
 ###### apply action
 gain = 0                            # setting a variable to use later on
+last_rule = pd.Series()             # inital last_rule is empty Series
 # remove (for debugging (this should be created in apply action))
 # last_rule = pd.read_csv("/home/osboxes/python_files/LCT/last_rule.csv")
-last_rule = pd.Series().astype("float64")       # inital last_rule is empty Series
 
-# could also specify initial fitness here
-# init_fit = ?
-# would then have to add code to put it in the rule_pop df
 
 ###### get rule population from a csv
-# rule_pop = pd.read_csv("rule_pop.csv")
-rule_pop = pd.read_csv("/home/osboxes/python_files/LCT/rule_pop.csv").astype("float64")
+# rule_pop = pd.read_csv("LCT/rule_pop/rule_pop_test.csv")
+# rule_pop = pd.read_csv("packages/lct/src/rule_pop.csv")         #change LCT to . when running in interactive
+rule_pop = make_rule_pop(10)
 
-#%% 
 # ROS SUBSCRIBER
 # ROS subriber to the lane_control_node & lane_filter topics
 
 ###### dummy vars for testing 
 # from velocity message (lane_controller)
-v = 3                                # velocity
+# v = 3                                # velocity
 
 # from lane_pose message (lane_filter)
-d = 0.1                                # lateral offset
-phi = 0                                 # angular offset
+# d = 0.1                                # lateral offset
+# phi = 0                                 # angular offset
 
-#%% 
-# CONSTANTS CLASS
+###### UPDATE: now arguments
+
 # The constants class saves the variables set in the 'Configuration file' as global constants
 class Constants:
     # def __init__(self):
@@ -82,11 +80,10 @@ class Constants:
         ## apply action
         self.gain = gain
 
+        self.counter = 0            # for debugging testing large rule pops
+
         # tbc
-
-# cons = Constants()
-
-#%% 
+#%%
 # LCT CLASS
 # LCT class contains the functions for updating fitness, action selection, and applying the action
 # based on c code for implementing LCT 
@@ -95,11 +92,11 @@ class LCT:
         return "This is the LCT class"
     # def __init__(self): 
         # cons = Constants()
-    def updatefitness(self):
+    def updatefitness(self, d, phi, v):
         print("Running fitness update")
         if cons.last_rule.empty:
             print("No action was selected last instance")
-            for i, rule in cons.rule_pop.iterrows():                # iterate through each rule        # 
+            for i, rule in cons.rule_pop.iterrows():                # iterate through each rule 
                     print(f"rule {i}")
                     # print(rule["in_match_set"])
                     rule["in_match_set"] = 0                        # reset in_match_set to false
@@ -107,17 +104,17 @@ class LCT:
         else:
             reward = 0
             # rewarding for fast speed
-            if objective_select == "vel_first" and cons.min_v <= v <= cons.max_v:             # where vel is from ROS node
+            if objective_select == "vel_first" and cons.min_v <= v <= cons.max_v:
                 print("rewarded for fast speed")
-                reward = 2 
+                reward = 10 # +ve val
             # punishment for slow speed
-            elif objective_select == "vel_first" and 0 <= v <= cons.min_v:
+            if objective_select == "vel_first" and 0 <= v <= cons.min_v:
                 print("punished for slow speed")
                 reward = -10 # -ve val
             # punishment for crashing (going really slow)
-            elif objective_select == "vel_first" and 0 <= v <= cons.stop_v:
+            if objective_select == "vel_first" and 0 <= v <= cons.stop_v:
                 print("punished for crashing")
-                reward = -20 # -ve val
+                reward = -20 # -ve val (bigger)
                         
             for i, rule in cons.rule_pop.iterrows():             # iterate through each rule 
                 
@@ -134,26 +131,26 @@ class LCT:
                 # 
                 
                 # if "in_match_set" in rule:                    # check if the rule has a match set column
-                if rule["in_match_set"] == 1:                   # if the value of in_match_set is True
+                if cons.rule_pop["in_match_set"][i] == 1:                   # if the value of in_match_set is True
                     
                     # remove (here for debugging)
-                    # print(f'rule act: {rule["act"]} and last rule act: {cons.last_rule["act"]}')
+                    print(f'rule act: {rule["act"]} and last rule act: {cons.last_rule["act"]}')
                     #
                     
-                    if rule["act"] == cons.last_rule["act"]: 
+                    if cons.rule_pop["act"][i] == cons.last_rule["act"]: 
                         print(f"Rule {i} has been updated")
-                        rule["fit"] = rule["fit"] + (beta * (reward - rule["fit"]))
-                        rule["in_match_set"] = 0
+                        cons.rule_pop["fit"][i] = cons.rule_pop["fit"][i] + (beta * (reward - rule["fit"]))
+                        cons.rule_pop["in_match_set"][i] = 0
                     else:
                         print(f"The action of the current rule (Rule {i}) does not equal the action of the last rule")
-                        rule["in_match_set"] = 0
+                        cons.rule_pop["in_match_set"][i] = 0
                     
-    def actionselection(self):
+    def actionselection(self, d, phi, v):
         if algo_select == 'winner_takes_all':
             for i, rule in cons.rule_pop.iterrows():             # iterate through each rule
                 print(f"Current Rule: {i}")
                 rule_vel_match = rule["vel_lower"] <= v <= rule["vel_upper"]        # true if velocity in range
-                # print(f'vel_lower: {rule["vel_lower"]}, vel_upper{rule["vel_upper"]}')
+                print(f'vel_lower: {rule["vel_lower"]}, vel_upper{rule["vel_upper"]}')
                 rule_d_match = rule["d_lower"] <= d <= rule["d_upper"]              # true if d in range
                 rule_phi_match = rule["phi_lower"] <= phi <= rule["phi_upper"]      # true if phi in range
                 rule_match = rule_vel_match and rule_d_match and rule_phi_match
@@ -164,11 +161,13 @@ class LCT:
                 # cons.best_fitness = rule["fit"]
                 #
 
-                if rule_match:              # need == True ?
-                    rule["in_match_set"] = 1
-                    if cons.best_fitness < rule["fit"]:
+                if rule_match:
+                    cons.rule_pop["in_match_set"][i] = 1                # this isnt working 
+                    print(f'match set:{rule["in_match_set"]}')
+                    # cons.counter += 1
+                    if cons.best_fitness < cons.rule_pop["fit"][i]:
                         cons.selected_rule = i
-                        cons.best_fitness = rule["fit"]
+                        cons.best_fitness = cons.rule_pop["fit"][i]
         
         elif algo_select == "roulette_wheel":
             acc_weight_list = [] 
@@ -189,7 +188,7 @@ class LCT:
                 #
 
                 if rule_match:
-                    rule["in_match_set"] = 1
+                    cons.rule_pop["in_match_set"][i] = 1                # cons.rule_pop["in_match_set"][i] = 1
                     fitness_sum += rule["fit"]
                     acc_weight_list.append(fitness_sum)
                     print(f"fitness sum: {fitness_sum} & weight list: {acc_weight_list}")
@@ -200,18 +199,18 @@ class LCT:
                 rand_number = random.random()
                 rand_number = rand_number * fitness_sum
                 for i, rule in cons.rule_pop.iterrows():
-                    if rule["in_match_set"] == 1:
+                    if cons.rule_pop["in_match_set"][i] == 1:
                         print(f"rule {i} matches")
                         print(f"the random number is {rand_number}")
                         if rand_number <= acc_weight_list[i]:
                             cons.selected_rule = i
-                            cons.best_fitness = rule["fit"]
+                            cons.best_fitness = cons.rule_pop["fit"][i]
                         
 
         print(f"Best fitness is {cons.best_fitness} and the selected rule is {cons.selected_rule}")    
         return cons.best_fitness, cons.selected_rule 
                     
-    def applyaction(self):
+    def applyaction(self, d, phi, v):
         if cons.selected_rule == -1:                                # no rule selected so nothing's done
             print("No rule selected")
             cons.last_rule = pd.Series().astype("float64")          # create empty series
@@ -228,13 +227,9 @@ class LCT:
             # remove (for debugging)
             # print(f"Last rule: {cons.last_rule}")
             #
+            cons.counter += 1
         return cons.gain   
 
-
-#%% ROS PUBLISHER
-# ROS publisher for applying the action 
-
-#%% 
 # run LCT class
 # class for running the LCT (runs the methods in the LCT class, add timing here?)
 class runLCT:
@@ -242,22 +237,27 @@ class runLCT:
         print("Initialize LCT algorithm")
         #...
     
-        self.run_LCT()
+        #self.run_LCT()
 
-    def run_LCT(self):
+    def run_LCT(self, d, phi, v):
         cons = Constants()
-        LCT().updatefitness()
-        LCT().actionselection()
-        LCT().applyaction()
-        """
-        for i in range(3): # set up a loop that decides how long to run
-            LCT().updatefitness()
-            LCT().actionselection()
-            LCT().applyaction()
-        """
+        LCT().updatefitness(d, phi, v)
+        LCT().actionselection(d, phi, v)
+        LCT().applyaction(d, phi, v)
+        return 
 
 # %%
+# here is what runs the code
+"""
 cons = Constants()
 cons.setConstants()
-runLCT()
+for i in range(1000):              # choose no of iters
+    print(f"iter {i}")
+    runLCT().run_LCT(0.1, 0, 3)
+
+print(cons.rule_pop)
+print(f"max fit: {cons.rule_pop.max()['fit']} & min fit: {cons.rule_pop.min()['fit']}")
+print(f"counter: {cons.counter}")
+"""
 # %%
+# note to self remove all print statements after testing 
